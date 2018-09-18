21d76af5896f6cb57e2bd1ca7e7414fd51c7bd49
Orion Angus <codenamedorion@gmail.com>
2018-09-17 20:17:51 -0400

    Added random movie and tomorrow's movie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import random
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
@@ -8,7 +8,7 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
-
+    movie2 = get_random_movie()
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
@@ -17,13 +17,20 @@ def index():
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    content2 = "<h1>Tommorrow's Movie</h1>"
+    content2 += "<ul>"
+    content2 += "<li>" + movie2 + "</li>"
+    content2 += "</ul>"
 
-    return content
+    return (content + content2)
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movies = ['MI1', 'MI2', 'MI3', 'MI4', 'MI5']
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movie_length = len(movies)
+    choice = movies[random.randrange(0,movie_length)]
+    return choice
 
 
 app.run()
